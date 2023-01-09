362dc344584ec451124cd9c226061983a2592ee9
Jacob <jbrow4@gmail.com>
2023-01-09 17:12:07 -0500

    Added general info

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { redirect, useLocation } from "react-router-dom";
 import {
   Link as RouterLink,
 } from 'react-router-dom';
-import {Navigate, Outlet} from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import { createTheme } from "@mui/material/styles";
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import PaymentsIcon from '@mui/icons-material/Payments';
+import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
+
+export const primaryMain = '#4c809e'
+export const secondaryMain = '#d68758'
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: primaryMain
+    },
+    secondary: {
+      main: secondaryMain
+    }
+  }
+})
 
 function App() {
   let location = useLocation();
   const [tabValue, setTabValue] = React.useState(location.pathname);
-
+  if (location.pathname === '/') { return <Navigate to="/main" /> }
   return (
-    <div className="App">
-      <Outlet/>
-      <BottomNavigation
-                  showLabels
-                  value={tabValue}
-                  onChange={(event, newValue: string) => {
-                    setTabValue(newValue);
-                    redirect("/" + newValue)
-                  }}>
-                <BottomNavigationAction label="Main" value='/main' component={RouterLink} to="main" />
-                <BottomNavigationAction label="About" value='/about' component={RouterLink} to="about" />
-                <BottomNavigationAction label="Pricing" value='/pricing' component={RouterLink} to="pricing" />
-                <BottomNavigationAction label="Contact" value='/contact' component={RouterLink} to="contact" />
-              </BottomNavigation>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box component="main"
+          sx={{ width: '100%', p: 2, mb: 8, height: '100%' }}>
+          <Box>
+            <Typography variant='h5'>
+              Photos by Engineer It
+            </Typography>
+          </Box>
+          <Outlet />
+        </Box>
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={5}>
+          <BottomNavigation
+            showLabels
+            value={tabValue}
+            onChange={(event, newValue: string) => {
+              setTabValue(newValue);
+              redirect("/" + newValue)
+            }}>
+            <BottomNavigationAction label="Main" value='/main' component={RouterLink} to="main" icon={<HomeIcon />} />
+            <BottomNavigationAction label="About" value='/about' component={RouterLink} to="about" icon={<InfoIcon />} />
+            <BottomNavigationAction label="Pricing" value='/pricing' component={RouterLink} to="pricing" icon={<PaymentsIcon />} />
+            <BottomNavigationAction label="Contact" value='/contact' component={RouterLink} to="contact" icon={<PermContactCalendarIcon />} />
+          </BottomNavigation>
+        </Paper>
+      </ThemeProvider>
+    </>
   );
 }
 

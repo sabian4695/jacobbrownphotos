4fb1870f82148a5c7457c5c27ff604f09230ecd9
Jacob Brown <jbrow4@gmail.com>
2023-02-24 15:00:11 -0500

    correcting favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return(
     <Html lang="en">
-      <Head charSet="utf-8" />
+      <Head charSet="utf-8">
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main/>
         <NextScript/>

63116f39a45b187f3d3eeea5f263d6b48fd17064
Jacob Brown <jbrow4@gmail.com>
2025-05-27 15:27:53 -0400

    adding more photos

--- src/components/allPhotos.js
+++ src/components/allPhotos.js
@@ -10,10 +10,21 @@ import family4 from '../../public/images/family4.jpeg'
 import family5 from '../../public/images/family5.jpeg'
 import family6 from '../../public/images/family6.jpeg'
 import family7 from '../../public/images/family7.jpeg'
+import family10 from '../../public/images/family10.jpeg'
 import family11 from '../../public/images/family11.jpeg'
 import family12 from '../../public/images/family12.jpeg'
 import family13 from '../../public/images/family13.jpeg'
 import family14 from '../../public/images/family14.jpeg'
+import family15 from '../../public/images/family15.jpeg'
+import family16 from '../../public/images/family16.jpeg'
+import family17 from '../../public/images/family17.jpeg'
+import family18 from '../../public/images/family18.jpeg'
+import family19 from '../../public/images/family19.jpeg'
+import family20 from '../../public/images/family20.jpeg'
+import family21 from '../../public/images/family21.jpeg'
+import family22 from '../../public/images/family22.jpeg'
+import family23 from '../../public/images/family23.jpeg'
+import family24 from '../../public/images/family24.jpeg'
 import baby0 from '../../public/images/baby0.jpeg'
 import baby1 from '../../public/images/baby1.jpeg'
 import baby2 from '../../public/images/baby2.jpeg'
@@ -43,8 +54,16 @@ import portrait0 from '../../public/images/portrait0.jpeg'
 import portrait1 from '../../public/images/portrait1.jpeg'
 import portrait2 from '../../public/images/portrait2.jpeg'
 import portrait3 from '../../public/images/portrait3.jpeg'
+import portrait4 from '../../public/images/portrait4.jpeg'
+import portrait5 from '../../public/images/portrait5.jpeg'
+import portrait6 from '../../public/images/portrait6.jpeg'
+import portrait7 from '../../public/images/portrait7.jpeg'
 import food0 from '../../public/images/food0.jpeg'
 import food1 from '../../public/images/food1.jpeg'
+import food2 from '../../public/images/food2.jpeg'
+import food3 from '../../public/images/food3.jpeg'
+import food4 from '../../public/images/food4.jpeg'
+import food5 from '../../public/images/food5.jpeg'
 import event1 from '../../public/images/event1.jpeg'
 import event2 from '../../public/images/event2.jpeg'
 import event3 from '../../public/images/event3.jpeg'
@@ -67,6 +86,19 @@ import event19 from '../../public/images/event19.jpeg'
 import event20 from '../../public/images/event20.jpeg'
 import event21 from '../../public/images/event21.jpeg'
 import event22 from '../../public/images/event22.jpeg'
+import event23 from '../../public/images/event23.jpeg'
+import event24 from '../../public/images/event24.jpeg'
+import event25 from '../../public/images/event25.jpeg'
+import event26 from '../../public/images/event26.jpeg'
+import event27 from '../../public/images/event27.jpeg'
+import event28 from '../../public/images/event28.jpeg'
+import location0 from '../../public/images/location0.jpeg'
+import location1 from '../../public/images/location1.jpeg'
+import location2 from '../../public/images/location2.jpeg'
+import location3 from '../../public/images/location3.jpeg'
+import location4 from '../../public/images/location4.jpeg'
+import location5 from '../../public/images/location5.jpeg'
+
 import pricingPage from '../../public/images/pricingPage.jpeg'
 
 export const itemData = [
@@ -126,12 +158,17 @@ export const itemData = [
     },
     {
         img: family6,
-        title: 'Baby Picture',
+        title: 'Baby boy in the fall',
         category: 'family',
     },
     {
         img: family7,
         title: 'Timeless Family Photos',
+        category: 'family',
+    },
+    {
+        img: family10,
+        title: 'Family10',
         category: 'family',
     },
     {
@@ -153,10 +190,50 @@ export const itemData = [
         img: family14,
         title: 'Family14',
         category: 'family',
+    },{
+        img: family15,
+        title: 'Family15',
+        category: 'family',
+    },{
+        img: family16,
+        title: 'Family16',
+        category: 'family',
+    },{
+        img: family17,
+        title: 'Family17',
+        category: 'family',
+    },{
+        img: family18,
+        title: 'Family18',
+        category: 'family',
+    },{
+        img: family19,
+        title: 'Family19',
+        category: 'family',
+    },{
+        img: family20,
+        title: 'Family20',
+        category: 'family',
+    },{
+        img: family21,
+        title: 'Family21',
+        category: 'family',
+    },{
+        img: family22,
+        title: 'Family22',
+        category: 'family',
+    },{
+        img: family23,
+        title: 'Family23',
+        category: 'family',
+    },{
+        img: family24,
+        title: 'Family24',
+        category: 'family',
     },
     {
         img: baby0,
-        title: 'Baby Picture',
+        title: 'Baby boy on Fall Leaves',
         category: 'family',
     },{
         img: baby0,
@@ -164,11 +241,11 @@ export const itemData = [
         category: 'baby',
     },{
         img: baby1,
-        title: 'Baby held by Mom',
+        title: 'Baby boy held by Mom',
         category: 'baby',
     },{
         img: baby2,
-        title: 'Baby on Moms Shoulder',
+        title: 'Baby boy on Moms Shoulder',
         category: 'baby',
     },{
         img: baby1,
@@ -290,12 +367,44 @@ export const itemData = [
         title: 'Senior Pic on Bench',
         category: 'senior',
     },{
+        img: portrait4,
+        title: 'portrait4',
+        category: 'senior',
+    },{
+        img: portrait5,
+        title: 'portrait5',
+        category: 'senior',
+    },{
+        img: portrait6,
+        title: 'portrait6',
+        category: 'senior',
+    },{
+        img: portrait7,
+        title: 'portrait7',
+        category: 'senior',
+    },{
         img: food0,
         title: 'Product Photography',
         category: 'product',
     },{
         img: food1,
         title: 'Product Photos',
+        category: 'product',
+    },{
+        img: food2,
+        title: 'food2',
+        category: 'product',
+    },{
+        img: food3,
+        title: 'food3',
+        category: 'product',
+    },{
+        img: food4,
+        title: 'food4',
+        category: 'product',
+    },{
+        img: food5,
+        title: 'food5',
         category: 'product',
     },{
         img: event1,
@@ -386,8 +495,56 @@ export const itemData = [
         title: 'Ceremony Looking at Bride',
         category: 'event',
     },{
+        img: event23,
+        title: 'Empty Ceremony Location from side',
+        category: 'event',
+    },{
+        img: event24,
+        title: 'White Button Wedding Dress',
+        category: 'event',
+    },{
+        img: event25,
+        title: 'Basket of Beer Cans',
+        category: 'event',
+    },{
+        img: event26,
+        title: 'Ceremony at Combustion Brewery',
+        category: 'event',
+    },{
+        img: event27,
+        title: 'Ceremony Wide Angle from back',
+        category: 'event',
+    },{
+        img: event28,
+        title: 'Older kid holding basket down the aisle',
+        category: 'event',
+    },{
         img: pricingPage,
         title: 'Couple gazing into each others eyes at ceremony',
         category: 'Page Assets',
+    },{
+        img: location0,
+        title: 'location0',
+        category: 'location',
+    },{
+        img: location1,
+        title: 'location1',
+        category: 'location',
+    },{
+        img: location2,
+        title: 'location2',
+        category: 'location',
+    },{
+        img: location3,
+        title: 'location3',
+        category: 'location',
+    },{
+        img: location4,
+        title: 'location4',
+        category: 'location',
+    },{
+        img: location5,
+        title: 'location5',
+        category: 'location',
     }
 ];
